bfa659a17f053feee1cedb16e3a3c662a81f4bea
Jiyoon <72876243+jiyoon0523@users.noreply.github.com>
2024-05-29 17:34:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,15 @@
 console.log('test')
 
 document.getElementById('navigateButton1').addEventListener('click', function() {
-    window.location.href = 'unitydl://mylink';
+    window.location.href = 'unitydl://mylink/';
 });
 
 document.getElementById('navigateButton2').addEventListener('click', function() {
-    window.location.href = 'unitydl://mylink?FetchScene';
+    window.location.href = 'unitydl://mylink?FetchScene/';
 });
 
 document.getElementById('navigateButton3').addEventListener('click', function() {
-    window.location.href = 'unitydl://mylink?XRScene';
+    window.location.href = 'unitydl://mylink?XRScene/';
 });
 
 document.getElementById('navigateButton4').addEventListener('click', function() {
